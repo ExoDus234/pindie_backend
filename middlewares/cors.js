5670cbbcc3742048d01ccde346fadd0116b2880a
ExoDus234 <ivanpopkov06022007@gmail.com>
2024-05-17 17:35:11 +0700

    review number 2

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -1,7 +1,7 @@
 function cors(req, res, next) {
     const { origin } = req.headers;
 
-    if (allowedCors.includes(origin)) { // Если это наш друг
+    if (allowedCors.includes(origin)) {
         res.header('Access-Control-Allow-Origin', origin);
     }
 
@@ -9,9 +9,9 @@ function cors(req, res, next) {
 }
 
 const allowedCors = [
-    'https://practicum.yandex.ru',
-    'https://students-projects.ru',
-    'http://localhost:3000'
+    'https://practicum.yandex.ru/',
+    'http://practicum.yandex.ru/',
+    'localhost:3000'
 ];
 
 module.exports = cors;
